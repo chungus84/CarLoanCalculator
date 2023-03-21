7ae2d3cc773fa4c5f5eb5ca6bf63ecab8c09a3ff
kenichi beveridge <kenbev84@gmail.com>
2023-03-21 16:29:32 +0000

    Moved variables into main method and added if & else if

--- src/CarLoanCalculator.java
+++ src/CarLoanCalculator.java
@@ -1,9 +1,16 @@
 public class CarLoanCalculator {
     /** class to calculate a persons payment for a car loan*/
+    public static void main(String[] args) {
 
-    // class variables
-    int carLoan = 10000;
-    int loanLength = 3;
-    int interestRate = 5;
-    int dwnPayment = 2000;
+        int carLoan = 10000;
+        int loanLength = 3;
+        int interestRate = 5;
+        int downPayment = 2000;
+
+        if (loanLength <= 0 || interestRate <= 0) {
+            System.out.println("Error! You must take out a valid car loan.");
+        } else if (downPayment > carLoan) {
+            System.out.println("The car can be payed in full");
+        }
+    }
 }
