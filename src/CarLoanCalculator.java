a05b98db7b681e142d2ec904c10693add89f5aff
kenichi beveridge <kenbev84@gmail.com>
2023-03-21 17:04:04 +0000

    Refactored all into class methods

--- src/CarLoanCalculator.java
+++ src/CarLoanCalculator.java
@@ -1,23 +1,59 @@
 public class CarLoanCalculator {
     /** class to calculate a persons payment for a car loan*/
-    public static void main(String[] args) {
 
-        int carLoan = 10000;
-        int loanLength = 3;
-        int interestRate = 5;
-        int downPayment = 2000;
+    // class variables
+    int carLoan;
+    int loanLength;
+    int interestRate;
+    int downPayment;
+
+    // Constructor
+    public CarLoanCalculator(int loanAmount, int loanLengthYears, int loanInterestRate, int loanDownPayment) {
+        carLoan = loanAmount;
+        loanLength = loanLengthYears;
+        interestRate = loanInterestRate;
+        downPayment = loanDownPayment;
 
         if (loanLength <= 0 || interestRate <= 0) {
             System.out.println("Error! You must take out a valid car loan.");
         } else if (downPayment > carLoan) {
             System.out.println("The car can be payed in full");
-        } else {
-            int remainingBalance = carLoan - downPayment;
-            int months = loanLength * 12;
-            int monthlyBalance = remainingBalance / months;
-            int interest = monthlyBalance * interestRate / 100;
-            int monthlyPayment = monthlyBalance + interest;
-            System.out.println(monthlyPayment);
         }
+    }
+
+    public int calcRemainingBalance() {
+        /** class method returns the remaining balance */
+        return carLoan - downPayment;
+    }
+
+    public int monthsRemaining() {
+        /** class method returns the number of months remaining for the car loan duration */
+        return loanLength * 12;
+    }
+
+    public int calcMonthlyBalance(int balance, int remMonths) {
+        /** calculates the monthly balance for a loan W/O interest*/
+        return balance / remMonths;
+    }
+
+     public int calcInterest(int monthlyBal) {
+        /** calculates the interest amount per month */
+        return monthlyBal * interestRate / 100;
+     }
+
+     public int calcMonthlyPayment() {
+        /** calculates the monthly payment per month including interest */
+        int monthlyBalance = calcMonthlyBalance(calcRemainingBalance(), monthsRemaining());
+        int interest = calcInterest(monthlyBalance);
+        return monthlyBalance + interest;
+     }
+
+
+    public static void main(String[] args) {
+
+        // Instance to test should come to 233
+        CarLoanCalculator loanee1 = new CarLoanCalculator(10000, 3, 5, 2000);
+        System.out.println(loanee1.calcMonthlyPayment());
+
     }
 }
